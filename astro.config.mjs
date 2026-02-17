4369f34b8583843a407331637de51c902671a698
Dok92 <doslovdok@gmail.com>
2026-02-17 17:22:27 +0100

    Updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,6 @@ export default defineConfig({
     partytown({
       config: {
         forward: ['dataLayer.push'],
-        exclude: ['SharedStorage', 'AttributionReporting'],
       },
     }),
   ],

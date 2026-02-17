dc4fc0d03e48dfb24b6f907d0d5362dd46f7c229
Dok92 <doslovdok@gmail.com>
2026-02-17 16:29:45 +0100

    Updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,8 @@
 // @ts-nocheck
 import { defineConfig } from 'astro/config';
 
-import solidJs from '@astrojs/solid-js';
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   devToolbar: {
     enabled: false,
   },

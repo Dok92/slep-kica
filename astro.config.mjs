b1d86396f6f415fc528c7d10da59423b3925dc11
Dok92 <doslovdok@gmail.com>
2026-02-18 17:44:20 +0100

    Updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-nocheck
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
